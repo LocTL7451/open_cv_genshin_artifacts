1a25223f1ac80043847f13c5030217fb5a00dce2
IonHaryono <ion.haryono@gmail.com>
2022-11-14 15:19:04 +1100

    Added artifact folder + main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import cv2 as cv
 import numpy as np
 
-def readArtifact(img):
+def readArtifact(imgFinal):
+    
+
+
+    pass
+
+
+def processImage():
+
+
+
+    pass
+
+flower = cv.imread(artifacts/flower.jpg)
+cv.imshow('flower artifact', flower)
+# processImage(readArtifact(flower = cv.imread(artifacts/flower.jpg)))
